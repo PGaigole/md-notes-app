e548597c20f20d0883af7474150d9f6bc9a6efd0
Poonam Gaigole <poonam.gaigole@axelerant.com>
2024-11-08 15:06:43 +0530

    Removing firebase configs

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBR6z0o-dKZZ_A5OY0rYb9HXRwfmK8RAQY",
-  authDomain: "rnotes-623e7.firebaseapp.com",
-  projectId: "rnotes-623e7",
-  storageBucket: "rnotes-623e7.firebasestorage.app",
-  messagingSenderId: "117918953825",
-  appId: "1:117918953825:web:5993264babd7999ced9520",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
